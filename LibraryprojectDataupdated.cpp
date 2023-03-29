3cce4381b7419bdf6a4ce10c79cf2ce9b8d1377e
Mihaivosne <126684106+Mihaivosne@users.noreply.github.com>
2023-03-29 10:52:13 +0200

    Update LibraryprojectDataupdated.cpp

--- LibraryprojectDataupdated.cpp
+++ LibraryprojectDataupdated.cpp
@@ -1,14 +1,10 @@
-//============================================================================
-// Name        : Library.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+class User {
+public:
+    std::string username;
+    std::vector<std::shared_ptr<Media>> borrowedMedia;
 
-#include <iostream>
-#include <fstream>
-#include <string>
+    User(const std::string& username) : username(username) {}
+};
 
 class Data {
 public:
@@ -16,26 +12,64 @@ public:
     Data(const std::string& userDataFilename, const std::string& libraryDataFilename)
         : userDataFile(userDataFilename), libraryDataFile(libraryDataFilename) {}
 
-
-    void LoadLibrary() {
-        // Load library data from libraryDataFile
-    }
-
-    void SaveLibrary() {
-        // Save library data to libraryDataFile
-    }
+    
 
     void SaveUserData() {
-        // Save user data to userDataFile
+        std::ofstream outFile(userDataFile);
+
+        for (const auto& user : users) {
+            outFile << user.username << std::endl;
+            for (const auto& media : user.borrowedMedia) {
+                outFile << media->type << "   " << media->title << "   " << media->getCreator() << std::endl;
+            }
+            outFile << "###" << std::endl;
+        }
+
+        outFile.close();
     }
 
     void LoadUserData() {
-        // Load user data from userDataFile
+        std::ifstream inFile(userDataFile);
+        std::string line;
+
+        User* currentUser = nullptr;
+
+        while (std::getline(inFile, line)) {
+            if (line == "###") {
+                currentUser = nullptr;
+            } else if (currentUser == nullptr) {
+                users.push_back(User(line));
+                currentUser = &users.back();
+            } else {
+                std::vector<std::string> tokens;
+                std::string token;
+                size_t pos = 0;
+
+                while ((pos = line.find("   ")) != std::string::npos) {
+                    token = line.substr(0, pos);
+                    tokens.push_back(token);
+                    line.erase(0, pos + 3);
+                }
+                tokens.push_back(line);
+
+                if (tokens.size() == 3) {
+                    if (tokens[0] == "Book") {
+                        currentUser->borrowedMedia.push_back(std::make_shared<Book>(tokens[1], tokens[2]));
+                    } else if (tokens[0] == "CD") {
+                        currentUser->borrowedMedia.push_back(std::make_shared<CD>(tokens[1], tokens[2]));
+                    } else if (tokens[0] == "Film") {
+                        currentUser->borrowedMedia.push_back(std::make_shared<Film>(tokens[1], tokens[2]));
+                    }
+                }
+            }
+        }
+
+        inFile.close();
     }
 
 private:
-    // Attributes
     std::string userDataFile;
     std::string libraryDataFile;
+    std::vector<std::shared_ptr<Media>> library;
+    std::vector<User> users;
 };
-
