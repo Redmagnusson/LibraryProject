1307ac5183a64b9733cd3efc6f7227bc608f37f5
Mihaivosne <126684106+Mihaivosne@users.noreply.github.com>
2023-03-30 14:54:51 +0200

    Update Data.cpp

--- Data.cpp
+++ Data.cpp
@@ -1,10 +1,3 @@
-//class User {
-//public:
-//    std::string username;
-//    std::vector<std::shared_ptr<Media>> borrowedMedia;
-//
-//    User(const std::string& username) : username(username) {}
-//};
 #include <string>
 #include <fstream>
 #include <sstream>
@@ -19,84 +12,43 @@ public:
     Data(const std::string& userDataFilename, const std::string& libraryDataFilename)
         : userDataFile(userDataFilename), libraryDataFile(libraryDataFilename) {}
 
-    
+    // Other functions ...
 
-    void SaveUserData(/* std::string username, size_t password*/) {
-
-        //Make this function take username as string and a password as size_t and save them to a new line
-        //We want to call this function when we create a new account
-        std::ofstream outFile(userDataFile);
-
-        for (const auto& user : users) {
-            outFile << user.getUsername() << std::endl;
-            for (const auto& media : user.borrowedMedia) {
-                outFile << media->type << "   " << media->title << "   " << media->getCreator() << std::endl;
-            }
-            outFile << "###" << std::endl;
-        }
-
-        outFile.close();
-    }
-
-    void LoadUserData(/*std::string username*/) {
-        //Make this function search the userdata file and return the size_t password for a user
-        std::ifstream inFile(userDataFile);
+    void loadMedia(std::vector<std::shared_ptr<Media>>& media) {
+        std::ifstream inFile(libraryDataFile);
         std::string line;
 
-        User* currentUser = nullptr;
-
         while (std::getline(inFile, line)) {
-            if (line == "###") {
-                currentUser = nullptr;
-            } else if (currentUser == nullptr) {
-                users.push_back(User(line));
-                currentUser = &users.back();
-            } else {
-                std::vector<std::string> tokens;
-                std::string token;
-                size_t pos = 0;
+            std::stringstream ss(line);
+            std::string type, title, creator, loanedID;
+            int releaseYear;
 
-                while ((pos = line.find("   ")) != std::string::npos) {
-                    token = line.substr(0, pos);
-                    tokens.push_back(token);
-                    line.erase(0, pos + 3);
-                }
-                tokens.push_back(line);
+            ss >> type >> title >> releaseYear >> loanedID >> creator;
 
-                if (tokens.size() == 3) {
-                    if (tokens[0] == "Book") {
-                        currentUser->getLoanedBooks().push_back(Book(tokens[1], tokens[2]));
-                    } else if (tokens[0] == "CD") {
-                        currentUser->getLoanedBooks().push_back(CD(tokens[1], tokens[2]));
-                    } else if (tokens[0] == "Film") {
-                        currentUser->getLoanedBooks().push_back(Film(tokens[1], tokens[2]));
-                    }
-                }
+            if (type == "Book") {
+                media.push_back(std::make_shared<Book>(title, releaseYear, loanedID, creator));
+            } else if (type == "CD") {
+                media.push_back(std::make_shared<CD>(title, releaseYear, loanedID, creator));
+            } else if (type == "Film") {
+                media.push_back(std::make_shared<Film>(title, releaseYear, loanedID, creator));
             }
         }
 
         inFile.close();
     }
 
-    //Make a function to load all the media from the media text file into the vector ive called
-    void loadMedia(std::vector<Media>& media) {
+    void loadLoanedMedia(const std::string& username, std::vector<std::shared_ptr<Media>>& media) {
+        std::vector<std::shared_ptr<Media>> allMedia;
+        loadMedia(allMedia);
 
-        //Template on how to add media
-        // LoanedID is the username of the person that has loaned a book
-        // If the book has NOT been loaned, make it "" or something
-        // NEVER make a new Media object, only Film/CD/Book objects
-        //media.push_back(Book(title, releaseYear, loanedID, author));
-    }
-
-    //Make a function that loads all books loaned by a user and inserts it into the vector called
-    void loadLoanedMedia(std::vector<Media>& media) {
-
+        for (const auto& item : allMedia) {
+            if (item->getLoanedID() == username) {
+                media.push_back(item);
+            }
+        }
     }
 
 private:
     std::string userDataFile;
     std::string libraryDataFile;
-    //I dont think these 2 are needed for the new structure
-    //std::vector<std::shared_ptr<Media>> library;
-    //std::vector<User> users;
 };
